b350d6b7d45e14b3aed0c6afa018cfad04187a6a
Ashimkayal4 <ashimkayal4@gmail.com>
2024-09-23 02:44:45 +0600

    apply 1st function in this project

--- js/project.js
+++ js/project.js
@@ -1,3 +1,9 @@
 document.getElementById("open-donate-section").addEventListener('click', function () {
-    console.log('show-donate section clicked')
+    document.getElementById("show-donate-section").classList.remove('hidden');
+    document.getElementById('show-history-section').classList.add('hidden');
 });
+document
+  .getElementById("open-history-section").addEventListener("click", function () {
+    document.getElementById("show-donate-section").classList.add("hidden");
+    document.getElementById("show-history-section").classList.remove("hidden");
+  });
