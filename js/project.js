33d823ff35896742d77485a8dec82d4999dd9588
Ashimkayal4 <ashimkayal4@gmail.com>
2024-09-23 14:08:37 +0600

    added history functionallity and modal

--- js/project.js
+++ js/project.js
@@ -24,6 +24,7 @@ function getInputValueById(id) {
 }
 
 /* common function that works donation section*/
+
 function updateFundBalance(inputId, fundId, balanceId) {
   let inputNumber = getInputValueById(inputId);
 
@@ -32,12 +33,13 @@ function updateFundBalance(inputId, fundId, balanceId) {
     let total = currentFund + inputNumber;
     document.getElementById(fundId).innerText = total;
 
-    /* decrease main balance */
+    /* modal show set up in common function */ 
+    document.getElementById("my-modal").showModal();
 
+    /* decrease main balance */
     let accountBalance = getBtnValueById(balanceId);
     let accountBalanceLeft = accountBalance - inputNumber;
     document.getElementById(balanceId).innerText = accountBalanceLeft;
-
     
   }
   else {
@@ -45,30 +47,80 @@ function updateFundBalance(inputId, fundId, balanceId) {
   }
 }
 
-/* call the common donation function*/
+/* call the common donation function // section : 1 */
 document.getElementById("noakhali-donate-btn").addEventListener('click', function () {
   updateFundBalance(
     "noakhali-input-number",
     "noakhali-fund-number",
     "account-balance-number"
   );
-  document.getElementById("noakhali-input-number").value='';
+ 
+  let donateMoney = getInputValueById("noakhali-input-number");
+
+  if (donateMoney > 0 && !isNaN(donateMoney)) {
+    let div = document.createElement("div");
+    div.classList.add("border");
+    div.classList.add("rounded-md");
+    div.classList.add("mb-5");
+    div.classList.add("p-5");
+
+    div.innerHTML = `
+    <p class="font-semibold text-xl mb-3">${donateMoney} Taka is Donated for Flood Relief in Noakhali,Bangladesh</p>
+    <p>Date: ${Date()}</p>
+  `;
+   document.getElementById("show-history-section").appendChild(div);
+  }
+  
+  
 });
 
+
+/* call the common donation function // section : 2*/
 document.getElementById("feni-donation-btn").addEventListener('click', function () {
   updateFundBalance(
     "feni-input-number",
     "feni-fund-number",
     "account-balance-number"
   )
-  document.getElementById("feni-input-number").value = "";
+
+  let DonateMoney = getInputValueById("feni-input-number");
+
+  if (DonateMoney > 0 && !isNaN(DonateMoney)) {
+    let div = document.createElement('div');
+    div.classList.add('border')
+    div.classList.add('rounded-md')
+    div.classList.add('mb-5')
+    div.classList.add('p-5')
+    div.innerHTML = `
+    <p class="font-semibold text-xl mb-3">${DonateMoney} Taka is Donated for Flood Relief in Feni,Bangladesh</p>
+    <p>Date: ${Date()}</p>
+  `;
+  document.getElementById("show-history-section").appendChild(div);
+  }
+
+
 });
 
+
+/* call the common donation function // section : 3 */
 document.getElementById("protest-donation-btn").addEventListener('click', function () {
   updateFundBalance(
     "protest-input-number",
     "protest-fund-number",
     "account-balance-number"
   )
-  document.getElementById("protest-input-number").value = "";
+  let donate = getInputValueById("protest-input-number");
+  if (donate > 0 && !isNaN(donate)) {
+    let div = document.createElement("div");
+    div.classList.add("border");
+    div.classList.add("rounded-md");
+    div.classList.add("mb-5");
+    div.classList.add("p-5");
+
+    div.innerHTML = `
+    <p class="font-semibold text-xl mb-3">${donate} Taka is Donated for Aid for Injured in the Quota Movement, Bangladesh</p>
+    <p>Date: ${Date()}</p>
+  `;
+    document.getElementById("show-history-section").appendChild(div);
+  }
 });
